908667fd8d997b23ee6a056a477118ead3e3ed20
Daniel Oyewole <danieloyewole.engr@gmail.com>
2025-06-03 19:01:42 -0400

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -63,4 +63,7 @@ resource "aws_ecs_service" "app" {
 resource "aws_appautoscaling_target" "ecs" {
   max_capacity       = 4
   min_capacity       = 1
-  resource_id        = "service/${
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
